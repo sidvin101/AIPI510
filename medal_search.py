78b2a6464d2d4822fc228c2a28ff8f7355d1493e
Siddarth Vinnakota <77826041+sidvin101@users.noreply.github.com>
2024-08-26 19:41:28 -0400

    Update medal_search.py
    Changed the file a little bit to allow for unit testing

--- medal_search.py
+++ medal_search.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import argparse
 
-def main(search_term):
+#Function to collect medal counts
+def get_medal_counts(search_term):
 
-    # First, test if the input is empty
+    #If no search_term is recognized
     if not search_term:
         return "You have not entered a Country or Country Code. Please type one."
 
+    #Uppercase the search term
     search_term = search_term.upper()
 
+    #Extract the olympics dataset
     df = pd.read_csv("olympics2024.csv")
 
+    #Get a list of countries in the olympics
     df['Country'] = df['Country'].str.upper()
     countries = df['Country'].tolist()
     country_abr = df['Country Code'].tolist()
 
-
-    # Then, test if the input is not recognized
+    #Raise an error if the country does not exist 
     if search_term not in countries and search_term not in country_abr:
         return """
 I'm sorry, either this country does not exist or have not participated in this
@@ -24,19 +27,24 @@ year's Olympics. If you believe that this is not the case, please check your
 spelling. The full name of the country or the abbreviation are enough.
 """
 
-    # Otherwise, we are free to return the result
+    #Gets the row with the correct country or country abbreviation based on what the user input
     if search_term in countries:
         row = df.loc[(df['Country'] == search_term)]
     elif search_term in country_abr:
         row = df.loc[(df['Country Code'] == search_term)]
 
+        
+    #print out the medal count
     return f"{row['Country'].iloc[0]} has {row['Gold'].iloc[0]} Gold medals, {row['Silver'].iloc[0]} Silver medals, and {row['Bronze'].iloc[0]} Bronze medals. A total of {row['Total'].iloc[0]} medals."
 
-if __name__ == "__main__":
+def main():
+    #Adds an argument parser to allow the user to paste their search_term directly on the command line
     parser = argparse.ArgumentParser()
     parser.add_argument("search_term", type=str)
     args = parser.parse_args()
-
-    result = main(args.search_term)
+    result = get_medal_counts(args.search_term)
     if result:
         print(result)
+
+if __name__ == "__main__":
+    main()
